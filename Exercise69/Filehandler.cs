6e4bff69366b35357c003a169c905d1bd1703355
jason-aultman <jason.aultman@gmail.com>
2020-08-12 15:42:12 -0400

    initial commit

---
+++ Exercise69/Filehandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise69
+{
+    public class Filehandler
+    {
+        public void WriteFile(string filepath, string filename, List<int> intList)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(filepath+filename))
+            {
+                foreach (var VARIABLE in intList)
+                {
+                    streamWriter.WriteLine(VARIABLE);
+                }
+            }
+        }
+    }
+}
